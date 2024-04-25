575dd60cceee1c377d4b757ec2723b81a1bd0e97
vineethsankre <lsvineethsankre@gmail.com>
2024-04-25 07:53:38 +0000

    Replaced dummy blogsData using actual blogsData using fetch() and updated state

--- src/components/BlogsList/index.js
+++ src/components/BlogsList/index.js
@@ -4,27 +4,31 @@ import BlogItem from '../BlogItem'
 
 import './index.css'
 
-const blogsData = [
-  {
-    id: 1,
-    title: 'Blog 1',
-    imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-1-img.png',
-    avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
-    author: 'Author Name',
-    topic: 'React.js',
-  },
-  {
-    id: 2,
-    title: 'Blog 2',
-    imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-2-img.png',
-    avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
-    author: 'Author Name',
-    topic: 'React.js',
-  },
-]
-
 class BlogsList extends Component {
+  state = {
+    blogsData: [],
+  }
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async() => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const updatedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      topic: eachItem.topic,
+      author: eachItem.author,
+    }))
+    this.setState({blogsData: updatedData})
+  }
+
   render() {
+    const {blogsData} = this.state
     return (
       <div className="blog-list-container">
         {blogsData.map(item => (
