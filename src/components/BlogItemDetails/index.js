93414e81b197d2f274038119edb2f982fee84af5
vineethsankre <113631587+vineethsankre@users.noreply.github.com>
2024-04-25 15:01:09 +0530

    Update index.js
    Fetched blogData using fetch() and updated state

--- src/components/BlogItemDetails/index.js
+++ src/components/BlogItemDetails/index.js
@@ -2,17 +2,29 @@ import {Component} from 'react'
 
 import './index.css'
 
-const blogData = {
-  title: 'Blog Name',
-  imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-3-img.png',
-  avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
-  author: 'Author Name',
-  content:
-    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-}
-
 class BlogItemDetails extends Component {
+  state:{blogData:{}}
+  componentDidMount(){
+    this.getBlogData()
+  }
+  getBlogData = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+    const response = await fetch('https://apis.ccbp.in/blogs/${id}')
+    const data = response.json()
+    const updatedData = {
+      author: data.author,
+      title: data.title,
+      imageUrl: data.image_url,
+      avatarUrl: data.avatar_url,
+      content: data.content,
+      topic: data.topic,
+    }
+    this.setState({blogData: updatedData})
+  }
   renderBlogItemDetails = () => {
+    const {blogData} = this.state
     const {title, imageUrl, content, avatarUrl, author} = blogData
     return (
       <div className="blog-info">
